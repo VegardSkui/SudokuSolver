2284e56caa13ba64943c85a09e1fd4a84d7b328a
Vegard Skui <me@vegardskui.com>
2020-12-09 20:59:40 +0100

    Create Xcode project

---
+++ SudokuSolver/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SudokuSolver
+//
+//  Created by Vegard Skui on 09/12/2020.
+//  Copyright © 2020 Vegard Skui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SudokuSolver/SudokuSolverApp.swift
@@ -0,0 +1,18 @@
+//
+//  SudokuSolverApp.swift
+//  SudokuSolver
+//
+//  Created by Vegard Skui on 09/12/2020.
+//  Copyright © 2020 Vegard Skui. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SudokuSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
